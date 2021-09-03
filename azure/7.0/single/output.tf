ef016dd610b6c7e50ede280a93af8cd91f61fb76
John McDonough <jmcdonough@fortinet.com>
2021-09-03 13:29:08 -0400

    correct curl statement quoting

--- azure/7.0/single/output.tf
+++ azure/7.0/single/output.tf
@@ -1,3 +1,3 @@
 output "curl_command" {
-  value = "curl -k -H \"Authorization: Bearer ${random_string.random_apikey.id}\" https://${azurerm_public_ip.public_ip.ip_address}/api/v2/cmdb/system/global"
+  value = "curl -k -H 'Authorization: Bearer ${random_string.random_apikey.id}' https://${azurerm_public_ip.public_ip.ip_address}/api/v2/cmdb/system/global"
 }
